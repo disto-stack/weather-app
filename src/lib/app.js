ab83e902884aea4c2e6bff5a43c14f6aa2e01ad1
disto-stack <juan.ocampo.2501@gmail.com>
2020-12-03 09:34:37 -0500

    Loading animation added

--- src/lib/app.js
+++ src/lib/app.js
@@ -4,9 +4,6 @@ class App {
     constructor() {
         this.weather = new Weather();
 
-        this.infoContent = document.querySelector('#info-content');
-        this.loading = document.querySelector('#loading');
-
         // Render initial data
         this.loadInitial();
     }
@@ -22,24 +19,22 @@ class App {
     async getWeatherData(location) {
         this.data = await this.weather.getWeather(location);
 
-        this.infoContent.style.display = "none";
-        this.loading.style.display = "flex";
+        document.querySelector('#info-container').innerHTML = '';
     }
 
     renderElements() {       
         let dataHtml = `
-            <img alt="Weather state icon" id="weatherIcon" src="http://openweathermap.org/img/wn/${this.data.weather[0].icon}@4x.png">
-            <div id="weather-detail">
-                <h2 id="city">${this.data.name}</h2>
-                <p id="temperature">${Math.round(this.data.main.temp - 273.15)} °C</p>
-                <p id="state">${this.data.weather[0].main}</p>
+            <div id="info-content">
+                <img alt="Weather state icon" id="weatherIcon" src="http://openweathermap.org/img/wn/${this.data.weather[0].icon}@4x.png">
+                <div id="weather-detail">
+                    <h2 id="city">${this.data.name}</h2>
+                    <p id="temperature">${Math.round(this.data.main.temp - 273.15)} °C</p>
+                    <p id="state">${this.data.weather[0].main}</p>
+                </div>
             </div>
         `
 
-        document.querySelector('#info-content').innerHTML = dataHtml;
-    
-        this.loading.style.display = "none";
-        this.infoContent.style.display = "flex";
+        document.querySelector('#info-container').innerHTML = dataHtml;
     }
 }
 
